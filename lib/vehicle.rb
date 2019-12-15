ea782fe8b9a17fbb43bfee1748e555c8dde1aa73
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-15 16:40:51 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,13 +2,19 @@ class Vehicle
 
 attr_accessor :wheel_size, :wheel_number
 
-def initialize(wheel_size, wheel_number)
-  @wheel_size = wheel_size
-  @wheel_number = wheel_number
-end 
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end 
 
-def go 
-  
+  def go 
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end 
+end 
 
 
 
